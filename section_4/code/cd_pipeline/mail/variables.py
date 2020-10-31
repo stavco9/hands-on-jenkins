9e9196d4cd0fcbd27a3f2ee0d19bcfee5d31f992
stavco9 <stavco9@gmail.com>
2020-11-01 00:47:45 +0200

    Test Mail function

--- section_4/code/cd_pipeline/mail/variables.py
+++ section_4/code/cd_pipeline/mail/variables.py
@@ -4,4 +4,4 @@ to = "stavco9@gmail.com"
 tokenPath = os.environ['GMAIL_TOKEN']
 #tokenPath = os.environ['HOME'] + "/token.pickle"
 mailSubject = "Jenkins pipeline"
-mailBody = "Started Jenkins pipeline"
+mailBody = "Started Jenkins pipeline for build {} and branch {}".format(os.environ["BUILD_NUMBER"], os.environ["BRANCH_NAME"])

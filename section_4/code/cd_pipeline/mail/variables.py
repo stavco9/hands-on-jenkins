211b882b90bd581344dd9b0d849e5fe9e06172d7
stavco9 <stavco9@gmail.com>
2020-11-07 18:28:47 +0200

    Changed mail format Jenkinsfile

--- section_4/code/cd_pipeline/mail/variables.py
+++ section_4/code/cd_pipeline/mail/variables.py
@@ -1,7 +1,8 @@
 import os
+import sys
 
-to = "stavco9@gmail.com"
+to = sys.argv[1] #"stavco9@gmail.com"
 tokenPath = os.environ['GMAIL_TOKEN']
 #tokenPath = os.environ['HOME'] + "/token.pickle"
-mailSubject = "Jenkins pipeline"
-mailBody = "Started Jenkins pipeline for build {} and branch {}".format(os.environ["BUILD_NUMBER"], os.environ["BRANCH_NAME"])
+mailSubject = sys.argv[2] #"Jenkins pipeline"
+mailBody = sys.argv[3] #"Started Jenkins pipeline for build {} and branch {}".format(os.environ["BUILD_NUMBER"], os.environ["BRANCH_NAME"])

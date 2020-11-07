30388224351b5b96bdce650c6366fe58b3a803fc
stavco9 <stavco9@gmail.com>
2020-11-07 18:56:20 +0200

    Chaned mail parameters

--- section_4/code/cd_pipeline/mail/variables.py
+++ section_4/code/cd_pipeline/mail/variables.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-to = sys.argv[1] #"stavco9@gmail.com"
+to = os.environ['mailTo'] 
 tokenPath = os.environ['GMAIL_TOKEN']
 #tokenPath = os.environ['HOME'] + "/token.pickle"
-mailSubject = sys.argv[2] #"Jenkins pipeline"
-mailBody = sys.argv[3] #"Started Jenkins pipeline for build {} and branch {}".format(os.environ["BUILD_NUMBER"], os.environ["BRANCH_NAME"])
+mailSubject = os.environ['mailSubject'] #"Jenkins pipeline"
+mailBody = os.environ['mailBody'] #"Started Jenkins pipeline for build {} and branch {}".format(os.environ["BUILD_NUMBER"], os.environ["BRANCH_NAME"])
